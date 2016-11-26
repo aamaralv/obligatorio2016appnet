9dfea7683c9ef18c2c952ea847152417e64c149b
AAMARAL\aa <alag14@gmail.com>
2016-11-26 14:04:46 -0300

    Se agrega proyecto REST para lista licencias.

---
+++ WS Logic Persistance/WebRestApplication/Models/Prueba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRestApplication.Models
+{
+    public class Prueba
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
